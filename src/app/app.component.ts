7aa1efa64d39e90ecb65c669f66bc83a9234791d
cristian-g <cristian@cristiangonzalez.com>
2019-02-27 10:48:58 +0100

    Added sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from './../services/auth.service';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -12,12 +13,16 @@ import Auth0Cordova from '@auth0/cordova';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage:any = TabsPage;
+  public pages: Array<{title: string, component: any}>;
 
   constructor(
     platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen
+    splashScreen: SplashScreen,
+    public auth: AuthService,
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -30,5 +35,15 @@ export class MyApp {
         Auth0Cordova.onRedirectUri(url);
       }
     });
+
+    this.pages = [
+      { title: 'List', component: TabsPage }
+    ];
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
