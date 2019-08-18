1071c7aef9a1713b915edc5d1320ea0b39a7182a
cristian-g <cristian@cristiangonzalez.com>
2019-08-19 00:07:19 +0200

    Removed unused fields

--- src/models/vehicle.ts
+++ src/models/vehicle.ts
@@ -6,9 +6,6 @@ export class Vehicle {
     id: string;
     brand: string;
     model: string;
-    year: number;
-    price: number;
-    key: string;
     emails: Array<String>;
     actions: Array<Action>;
     balance: number;
